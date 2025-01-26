09d1a312e5f1da2ad568108878351c5e8fce87ff
Sergio <77530549+sergi0g@users.noreply.github.com>
2025-01-26 19:25:47 +0200

    CSS fixes and and content

--- src/app/components/pages/home.tsx
+++ src/app/components/pages/home.tsx
@@ -62,42 +62,44 @@ export default async function Home() {
         <h2 className="text-center text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl dark:text-neutral-200">
           Features
         </h2>
-        <div className="m-8 grid gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-          <Card title="Lorem Ipsum" icon={IconDownload}>
-            Cubilia facilisi inceptos metus sociosqu bibendum nisi cum. Dolor
-            proin litora hac taciti gravida magna ad.
+        <div className="lg:m-8 mt-8 grid gap-8 lg:grid-cols-3">
+          <Card title="Get going in minutes" icon={IconDownload}>
+            Just run the installation script which takes care of everything for
+            you. All you need is an 64 bit Linux system and some terminal
+            know-how. Most common Linux distros are supported.
           </Card>
-          <Card title="Dolor Sit Amet" icon={IconHandClick}>
-            Pharetra neque platea ullamcorper aliquet quis vivamus. Aliquam
-            pellentesque netus quis dolor malesuada adipiscing.
+          <Card title="Everything at your fingertips" icon={IconHandClick}>
+            Check on your system status, install apps, manage settings, backups
+            and more through the web dashboard in the most intuitive,
+            user-friendly way.
           </Card>
-          <Card title="Consectetur Adipiscing" icon={IconBolt}>
-            Feugiat condimentum himenaeos non sociosqu egestas congue. Lectus
-            nam aptent adipiscing metus quisque nulla.
-          </Card>
-          <Card title="Elit, Sed Do" icon={IconShoppingBag}>
-            Sociis habitasse nec tempus integer quis ipsum phasellus. Est
-            senectus senectus amet sem egestas ultricies himenaeos.
+          <Card title="Fully-equipped app store" icon={IconShoppingBag}>
+            Our collection of almost 300 apps gives you an enormous amount of
+            choices whether you're running local AI agents, a home media server,
+            or various other utilities.
           </Card>
         </div>
       </div>
       <div className="m-8 border-t border-gray-200 py-12 dark:border-gray-800">
-        <h2 className="px-8 text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl dark:text-neutral-200">
-          Simple app management
+        <h2 className="px-8 text-4xl font-bold tracking-tight text-neutral-800 dark:text-neutral-200">
+          Simple app management.
         </h2>
-        <div className="m-8 grid gap-12 md:grid-cols-2">
+        <div className="lg:m-8 mt-8 grid gap-12 lg:grid-cols-2">
           <div className="flex flex-col gap-8">
-            <Card title="Lorem Ipsum" icon={IconApps}>
-              Cubilia facilisi inceptos metus sociosqu bibendum nisi cum. Dolor
-              proin litora hac taciti gravida magna ad.
+            <Card title="Straightforward installation" icon={IconApps}>
+              No need to copy around config files and set up storage paths! Just
+              click the install button, complete the form with any required
+              configuration options and you're done!
             </Card>
-            <Card title="Dolor Sit Amet" icon={IconAdjustments}>
-              Pharetra neque platea ullamcorper aliquet quis vivamus. Aliquam
-              pellentesque netus quis dolor malesuada adipiscing.
+            <Card title="Make it your own" icon={IconAdjustments}>
+              Configure your apps through a form on the dashboard. Choose
+              between options to expose your app and more!
             </Card>
-            <Card title="Consectetur Adipiscing" icon={IconRestore}>
-              Feugiat condimentum himenaeos non sociosqu egestas congue. Lectus
-              nam aptent adipiscing metus quisque nulla.
+            <Card title="Built-in backup management" icon={IconRestore}>
+              Backing up and restoring app data is done through a dedicated
+              section on the app page. Additionally, you'll be asked if you want
+              to backup app data before updating an app, so you'll never mess
+              up!
             </Card>
           </div>
           <Image
@@ -108,42 +110,46 @@ export default async function Home() {
         </div>
       </div>
       <div className="m-8 border-t border-gray-200 py-12 dark:border-gray-800">
-        <h2 className="px-8 text-right text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl dark:text-neutral-200">
-          Something else
+        <h2 className="px-8 text-right text-4xl font-bold tracking-tight text-neutral-800 dark:text-neutral-200">
+          Secure and flexible.
         </h2>
-        <div className="m-8 grid gap-12 md:grid-cols-2">
+        <div className="lg:m-8 mt-8 grid gap-12 lg:grid-cols-2">
           <Image
             src={appstore_image}
             alt="Screenshot of App store"
             className="rounded-xl"
           />
           <div className="flex flex-col gap-8">
-            <Card title="Lorem Ipsum" icon={IconPuzzle}>
-              Cubilia facilisi inceptos metus sociosqu bibendum nisi cum. Dolor
-              proin litora hac taciti gravida magna ad.
+            <Card title="Extend and customize it" icon={IconPuzzle}>
+              While all apps come preconfigured with working defaults, you can
+              directly override parts of the docker compose files to get the
+              ideal setup. All customizations are persistent across updates.
             </Card>
-            <Card title="Dolor Sit Amet" icon={IconCircleArrowUp}>
-              Pharetra neque platea ullamcorper aliquet quis vivamus. Aliquam
-              pellentesque netus quis dolor malesuada adipiscing.
+            <Card title="Painless update management" icon={IconCircleArrowUp}>
+              Runtipi automatically checks for updates to itself and your apps
+              and will let you know on the dashboard. Updating Runtipi is made
+              easy with the CLI, while all you need to do for your apps is click
+              the update button!
             </Card>
-            <Card title="Consectetur Adipiscing" icon={IconLockCheck}>
-              Feugiat condimentum himenaeos non sociosqu egestas congue. Lectus
-              nam aptent adipiscing metus quisque nulla.
+            <Card title="Secure by design" icon={IconLockCheck}>
+              Everything - including Runtipi's own containers - runs in Docker
+              containers to ensure you have a secure enough environment by
+              default.
             </Card>
           </div>
         </div>
       </div>
       <div className="border-t border-gray-200 py-12 dark:border-gray-800">
         <h2 className="px-8 text-center text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl dark:text-neutral-200">
-          Community at the center
+          Community Driven.
         </h2>
-        <div className="m-8 grid gap-8 md:grid-cols-3">
+        <div className="m-8 grid gap-8 lg:grid-cols-3">
           <Card
             title="Free and Open-source"
             footer={
               <Button
                 href="https://github.com/runtipi/runtipi"
-                className="betterhover:hover:bg-neutral-900 bg-black text-white"
+                className="betterhover:hover:bg-neutral-900 w-full bg-black text-white md:w-auto"
               >
                 See the code
                 <GitHubIcon className="ml-2 size-6" />
@@ -176,14 +182,14 @@ export default async function Home() {
               <>
                 <Button
                   href="https://discord.gg/Bu9qEPnHsc"
-                  className="betterhover:hover:bg-[#4752c4] bg-[#5865f2] text-white"
+                  className="betterhover:hover:bg-[#4752c4] w-full bg-[#5865f2] px-4 text-white"
                 >
                   Join our Discord
                   <DiscordIcon className="ml-2 size-6" />
                 </Button>
                 <Button
                   href="https://forums.runtipi.io"
-                  className="betterhover:hover:bg-gray-700 betterhover:dark:hover:bg-gray-300 bg-black text-white dark:bg-white dark:text-black"
+                  className="betterhover:hover:bg-gray-700 betterhover:dark:hover:bg-gray-300 w-full bg-black px-4 text-white dark:bg-white dark:text-black"
                 >
                   Join our forums
                   <IconMessage className="ml-2 size-6" />
@@ -191,8 +197,9 @@ export default async function Home() {
               </>
             }
           >
-            Runtipi has a very active community on Discord and its (brand new)
-            forums. We need more content here
+            Runtipi's Discord server is the heart of our community, and we're
+            now expanding the conversation to our newly launched forums - join
+            in and help us build a thriving community!"
           </Card>
           <Card title="Get involved">
             We welcome and appreciate contributions from anyone who is
