9f7b3acee41499569d571351015bb880c4c0d984
Sergio <77530549+sergi0g@users.noreply.github.com>
2025-01-26 19:25:47 +0200

    CSS fixes and and content

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
   href: string;
@@ -9,15 +10,14 @@ interface ButtonProps {
 
 export default function Button({ href, className, children }: ButtonProps) {
   return (
-    <div className="w-full rounded-md md:w-auto">
-      <Link
-        href={href}
-        className={`flex w-full items-center justify-center rounded-md border border-transparent px-8 py-3 text-base font-medium no-underline transition-colors duration-200 md:px-10 md:py-3 md:text-lg md:leading-6 ${
-          className ?? ""
-        }`}
-      >
-        {children}
-      </Link>
-    </div>
+    <Link
+      href={href}
+      className={twMerge(
+        "flex items-center justify-center rounded-md border border-transparent px-8 py-3 text-base font-medium no-underline transition-colors duration-200 md:text-lg md:leading-6",
+        className,
+      )}
+    >
+      {children}
+    </Link>
   );
 }
