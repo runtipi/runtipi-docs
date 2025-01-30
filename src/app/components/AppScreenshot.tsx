3c8235db97aac480779605ecfc8de985916ff9bd
Sergio <77530549+sergi0g@users.noreply.github.com>
2025-01-30 21:14:29 +0200

    fix: apply some review suggestions

--- src/app/components/AppScreenshot.tsx
+++ src/app/components/AppScreenshot.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 
 import appstore_light from "../../../public/images/appstore.png";
 import appstore_dark from "../../../public/images/appstore-dark.png";
+import { twMerge } from "tailwind-merge";
+
+const styles =
+  "w-full max-w-none rounded-lg bg-gray-900 ring-1 shadow-xl ring-gray-400/10 lg:w-10/12";
+const altText = "Screenshot of Runtipi&apos;s appstore";
 
 export const AppScreenshot = () => {
   return (
@@ -10,14 +15,14 @@ export const AppScreenshot = () => {
       <Image
         priority
         src={appstore_light}
-        className="w-full max-w-none rounded-lg bg-gray-900 ring-1 shadow-xl ring-gray-400/10 lg:w-10/12 dark:hidden"
-        alt="Screenshot of Runtipi's appstore"
+        className={twMerge(styles, "dark:hidden")}
+        alt={altText}
       />
       <Image
         priority
         src={appstore_dark}
-        className="hidden w-full max-w-none rounded-lg bg-gray-900 ring-1 shadow-xl ring-gray-400/10 lg:w-10/12 dark:block"
-        alt="Screenshot of Runtipi's appstore"
+        className={twMerge(styles, "hidden dark:block")}
+        alt={altText}
       />
     </div>
   );
