2f2155b0ef11bd20585077fd462835dfceb22e15
Nicolas Meienberger <github@thisprops.com>
2025-05-16 22:23:10 +0200

    docs: remove adding a new app section

--- src/content/docs/contributing/_meta.ts
+++ src/content/docs/contributing/_meta.ts
@@ -2,9 +2,6 @@ export default {
   "running-locally": {
     title: "Running locally",
   },
-  "adding-a-new-app": {
-    title: "Adding a new app",
-  },
   "running-the-cli-locally": {
     title: "Running the CLI locally",
   },
